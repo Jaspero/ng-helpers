d111e071d34463892d237e96f543e50063bb8cb9
Filip Lauc <filip.lauc93@gmail.com>
2018-06-23 20:20:28 +0200

    fix(ArrayFromObject): proper format for publishing :bug:

--- projects/ng-helpers/src/pipes/array-from-object/array-from-object.pipe.ts
+++ projects/ng-helpers/src/pipes/array-from-object/array-from-object.pipe.ts
@@ -4,14 +4,10 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: 'jpArrayFromObject'
 })
 export class ArrayFromObjectPipe implements PipeTransform {
-  static format(value: Object) {
-    return Object.entries(value).map(entry => ({
+  transform(value: Object): any {
+    return Object['entries'](value).map(entry => ({
       key: entry[0],
       value: entry[1]
     }));
-  }
-
-  transform(value: Object): any {
-    return ArrayFromObjectPipe.format(value);
   }
 }
