abcc45c893fa9f29c8887cb9819cc986fc7f1728
Filip Lauc <filip.lauc93@gmail.com>
2018-08-06 11:20:02 +0200

    chore(ArrayFromObject): removed module/pipe since angular includes it now :fire:

--- projects/ng-helpers/src/pipes/array-from-object/array-from-object.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArrayFromObjectPipe } from './array-from-object.pipe';
-
-describe('ArrayFromObjectPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArrayFromObjectPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
