4ba81bcbbe2fa857450845acaa994c742d204cf4
Matej Maloča <maloca1992@gmail.com>
2019-06-04 14:16:40 +0200

    feat(FormTouchOnHover): FormTouchOnHoverDirective simplification closes #3
    BREAKING CHANGE: bump angular to v8

--- projects/ng-helpers/src/directives/form-touch-on-hover/form-touch-on-hover.directive.ts
+++ projects/ng-helpers/src/directives/form-touch-on-hover/form-touch-on-hover.directive.ts
@@ -13,44 +13,16 @@ type entry = FormGroup | FormArray;
   selector: '[jpFormTouchOnHover]'
 })
 export class FormTouchOnHoverDirective {
-  static markFormGroupTouched(formGroup: FormGroup | FormArray) {
-    (Object as any).values(formGroup.controls).forEach(control => {
-      control.markAsTouched();
-
-      if (control && control.controls) {
-        if (Array.isArray(control.controls)) {
-          control.controls.forEach(c =>
-            FormTouchOnHoverDirective.markFormGroupTouched(c)
-          );
-        } else {
-          for (const key in control.controls) {
-            if (control.controls.hasOwnProperty(key)) {
-              control.controls[key].markAsTouched();
-            }
-          }
-        }
-      }
-    });
-  }
-
-  /**
-   * Entry FormGroup which to iterate over
-   */
   @Input() jpFormTouchOnHover: entry | Array<entry>;
 
-  /**
-   * Outputs when form finish iterating
-   */
   @Output() jpFormTouched = new EventEmitter();
 
   @HostListener('mouseenter')
   enter() {
     if (Array.isArray(this.jpFormTouchOnHover)) {
-      this.jpFormTouchOnHover.forEach(form =>
-        FormTouchOnHoverDirective.markFormGroupTouched(form)
-      );
+      this.jpFormTouchOnHover.forEach(form => form.markAllAsTouched());
     } else {
-      FormTouchOnHoverDirective.markFormGroupTouched(this.jpFormTouchOnHover);
+      this.jpFormTouchOnHover.markAllAsTouched();
     }
 
     this.jpFormTouched.emit();
